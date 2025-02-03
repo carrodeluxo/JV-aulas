cda5a5fdd69821df1139b2ac3f48756c89565e2e
Henrick Ryan <henriqueryan011@gmail.com>
2025-02-03 15:48:48 -0300

    JV-aulas

---
+++ Atividade 11 .js
@@ -0,0 +1,10 @@
+const numero = [ 5 , 12 , 18 , 7 , 20 ]
+
+for(let ler = 0 ; ler <numero.length;){
+    ler=ler +1 ;
+    
+    if (numero [ler] >=10 ){
+        console.log(numero[ler])
+
+    }
+}
